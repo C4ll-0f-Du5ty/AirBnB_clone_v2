f0ea0b3bf982bac06a6a81b444eb2fe165a51b9b
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-03-19 22:18:54 +0200

    done

--- models/base_model.py
+++ models/base_model.py
@@ -1,48 +1,30 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-from uuid import uuid4
+import uuid
 from datetime import datetime
 
 
 class BaseModel:
     """A base class for all hbnb models"""
-    # def __init__(self, *args, **kwargs):
-    #     """Instatntiates a new model"""
-    #     if not kwargs:
-    #         from models import storage
-    #         self.id = str(uuid.uuid4())
-    #         self.created_at = datetime.now()
-    #         self.updated_at = datetime.now()
-    #         storage.new(self)
-    #     else:
-    #         kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-    #                                                  '%Y-%m-%dT%H:%M:%S.%f')
-    #         kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-    #                                                  '%Y-%m-%dT%H:%M:%S.%f')
-    #         del kwargs['__class__']
-    #         self.__dict__.update(kwargs)
-
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-        if len(kwargs) > 0:
-            # for key, value in kwargs.items():
-            #     if key == 'created_at' or key == 'updated_at':
-            #         setattr(self, key, datetime.fromisoformat(value))
-            #     elif key != '__class__':
-            #         setattr(self, key, value)
-            for key, value in kwargs.items():
-                if key == 'created_at' and isinstance(value, str):
-                    setattr(self, key, datetime.fromisoformat(value))
-                elif key == 'updated_at' and isinstance(value, str):
-                    setattr(self, key, datetime.fromisoformat(value))
-                elif key != '__class__':
-                    setattr(self, key, value)
-        else:
+        """Instatntiates a new model"""
+        if not kwargs:
             from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             storage.new(self)
+        else:
+            if 'updated_at' in kwargs and isinstance(kwargs['updated_at'],
+                                                     str):
+                kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if 'created_at' in kwargs and isinstance(kwargs['created_at'],
+                                                     str):
+                kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,8 +14,7 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        key = obj.to_dict()['__class__'] + '.' + obj.id
-        self.all().update({key: obj})
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
