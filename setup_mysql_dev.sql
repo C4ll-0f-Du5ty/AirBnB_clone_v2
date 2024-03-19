d5602e2b4893b2eb7a56d4edd3cacad80722af04
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-03-20 01:25:22 +0200

    done

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,5 +3,5 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES on hbnb_dev_db.* to 'hbnb_dev'@'localhost';
-GRANT SELECT PRIVILEGES on performance_schema.* to 'hbnb_dev'@'localhost';
+GRANT SELECT on performance_schema.* to 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
