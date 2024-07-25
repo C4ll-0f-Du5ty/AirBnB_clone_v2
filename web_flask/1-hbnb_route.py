150f13b2b884c71a9c4be3d1f95b4c330f02c7da
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-07-25 08:28:09 +0300

    done

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,5 +9,10 @@ def Hello():
     """Displaying My first sentence"""
     return "Hello HBNB!"
 
+@app.route("/hbnb", strict_slashes=False)
+def Hello1():
+    """Displaying my second Sentence"""
+    return "HBNB"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
