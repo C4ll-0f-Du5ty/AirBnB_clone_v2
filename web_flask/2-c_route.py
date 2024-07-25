624f44d3528971780a178b6096ceb96d0444121a
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-07-25 08:48:56 +0300

    done

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""App 2"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Hello():
+    """Displaying My first sentence"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def Hello1():
+    """Displaying my second Sentence"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def Hello1(text):
+    """Displaying my third Sentence"""
+    
+    return f"C {text.replace("_", " ")}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
