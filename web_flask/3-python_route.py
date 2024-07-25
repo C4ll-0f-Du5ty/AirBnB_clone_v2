b543e63c4d7bd3c18f206d67ced6c83d3abfaa92
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-07-25 09:11:01 +0300

    done

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""App 2"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Hello():
+    """Displaying My first Page"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HelloHBNB():
+    """Displaying my second Page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def Display_C(text):
+    """Displaying my third Page"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def Display_Python(text="is cool"):
+    """Displaying my Fourth Page"""
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
