0138358031adb1dab4eef664311be88a95d408eb
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-07-27 08:11:45 +0300

    done

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""App 2"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Hello():
+    """Displaying My first Page"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HelloHBNB():
+    """Displaying my second Page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def Display_C(text):
+    """Displaying my third Page"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
+def Display_Python(text="is cool"):
+    """Displaying my Fourth Page"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:text>", strict_slashes=False)
+def Display_Number(text):
+    """Displaying my Fourth Page"""
+    return f"{text} is a number"
+
+
+@app.route("/number_template/<int:text>", strict_slashes=False)
+def Display_HTML(text):
+    """Displaying my Fourth Page"""
+    return render_template("5-number.html", Number=text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
