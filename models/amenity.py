0ca06115e61c458017bd7a13245dc9ecc85efd89
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-07-28 09:45:17 +0300

    done

--- models/amenity.py
+++ models/amenity.py
@@ -7,6 +7,6 @@ from sqlalchemy.orm import relationship
 class Amenity(BaseModel):
     """ The amenity class, contains name """
     __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False, overlaps="amenities")
+    # name = Column(String(128), nullable=False)
+    # place_amenities = relationship("Place", secondary="place_amenity",
+    #                                viewonly=False, overlaps="amenities")
--- models/place.py
+++ models/place.py
@@ -5,13 +5,13 @@ from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 import models
 
 
-association_table = Table("place_amenity", Base.metadata,
-                          Column("place_id", String(60),
-                                 ForeignKey("places.id"),
-                                 primary_key=True, nullable=False),
-                          Column("amenity_id", String(60),
-                                 ForeignKey("amenities.id"),
-                                 primary_key=True, nullable=False))
+# association_table = Table("place_amenity", Base.metadata,
+#                           Column("place_id", String(60),
+#                                  ForeignKey("places.id"),
+#                                  primary_key=True, nullable=False),
+#                           Column("amenity_id", String(60),
+#                                  ForeignKey("amenities.id"),
+#                                  primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
